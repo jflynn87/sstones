f184df5ed275ad1e9d17b2321d70776f399d142e
John Flynn <jflynn87@hotmail.com>
2018-12-13 16:03:14 +0900

    admin

--- sstones/ss_app/admin.py
+++ sstones/ss_app/admin.py
@@ -4,6 +4,11 @@ from django.contrib import admin
 
 from ss_app.models import Event, Appointment, Days, TimeSlots, Staff, Client, Notes, FocusAreas
 
+class TimeSlotsAdmin(admin.ModelAdmin):
+    list_display = ['day', 'start_time', 'end_time', 'available', 'assigned_to']
+    list_filter = ['day', 'assigned_to']
+
+
 admin.site.register(Event)
 admin.site.register(Appointment)
 admin.site.register(Days)
